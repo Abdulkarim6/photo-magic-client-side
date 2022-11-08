3284a924df33a2a7d3b3baffe5dee6e5f8d12a2b
Abdulkarim6 <mdhossainjwel687@gmail.com>
2022-11-09 05:27:37 +0600

    environment variable setup done

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h1 className="text-5xl">This is login page</h1>
+        </div>
+    );
+};
+
+export default Login;
